0a1804439b55d9fbfa8aeb63e51e8bf96e2543fb
Ekin KARAKOÇ <ekinkarakoc@imecar.local>
2024-08-14 16:37:08 +0300

    loading-meals

---
+++ app/meals/loading.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./loading.module.css";
+
+const MealsLoading = () => {
+  return <h3 className={classes.loading}>Fethcing meals...</h3>;
+};
+
+export default MealsLoading;
